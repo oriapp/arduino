dd0ac1f42433244ea33f5c73ae373aa26187fb36
R C N <37003700+oriapp@users.noreply.github.com>
2020-02-01 18:54:58 +0200

    Create meter-index.ino

---
+++ meter-index.ino
@@ -0,0 +1,46 @@
+# include <LiquidCrystal.h>
+
+const int trigPin = 7;
+const int echoPin = 8;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup(){
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  lcd.clear();
+  long duration, inches, cm, mm;
+  pinMode(trigPin, OUTPUT); 
+  digitalWrite(trigPin, LOW); 
+delayMicroseconds(2000); 
+digitalWrite(trigPin, HIGH); 
+delayMicroseconds(1000); 
+digitalWrite(trigPin, LOW);
+pinMode(echoPin, INPUT); 
+duration = pulseIn(echoPin, HIGH);
+inches = microsecondsToInches(duration); 
+cm = microsecondsToCentimeters(duration);
+mm = microsecondsToMillimeters(duration);
+lcd.print(cm);
+lcd.print("cm, ");
+lcd.print(mm);
+lcd.print(" mm");
+delay(250);
+ //lcd.cursor();
+ //lcd.scrollDisplayLeft();
+ //delay(200);
+ //lcd.noCursor();
+ //delay(500);
+}
+
+
+long microsecondsToInches(long microseconds)
+{return microseconds / 74 / 2;}
+long microsecondsToCentimeters(long microseconds)
+{return microseconds / 29 / 2;}
+long microsecondsToMillimeters(long microseconds)
+{return microseconds / 2.9 / 2;}
